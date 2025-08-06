0a8270a4e50febc6d64f64b20f28dbc9fb080ca4
Yuvansh Singh <yuvansh.singh_cs23@gla.ac.in>
2025-08-06 08:26:59 +0530

    SortingRuntimeAnalysis.java Uploaded

---
+++ Algorithm's Runtime Analysis & Big-O Notation/SortingRuntimeAnalysis.java
@@ -0,0 +1,73 @@
+// 2. Sorting Large Data Efficiently: Bubble, Merge, Quick Sort
+public class SortingRuntimeAnalysis {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) return;
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+        System.arraycopy(arr, 0, left, 0, mid);
+        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) arr[k++] = left[i++];
+            else arr[k++] = right[j++];
+        }
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+    public static void compareSorts(int[] arr) {
+        int[] arr1 = arr.clone(), arr2 = arr.clone(), arr3 = arr.clone();
+        long start = System.nanoTime();
+        bubbleSort(arr1);
+        long bubbleTime = System.nanoTime() - start;
+        start = System.nanoTime();
+        mergeSort(arr2);
+        long mergeTime = System.nanoTime() - start;
+        start = System.nanoTime();
+        quickSort(arr3, 0, arr3.length - 1);
+        long quickTime = System.nanoTime() - start;
+        System.out.println("Bubble Sort Time: " + bubbleTime + " ns");
+        System.out.println("Merge Sort Time: " + mergeTime + " ns");
+        System.out.println("Quick Sort Time: " + quickTime + " ns");
+    }
+}
