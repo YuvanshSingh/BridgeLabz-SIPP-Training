1e5df410a7ced175ce4c5fce681f93c513c3ecd7
Yuvansh Singh <yuvansh.singh_cs23@gla.ac.in>
2025-08-06 08:27:41 +0530

    StringBuilderReverse.java Uploaded

---
+++ linear and binary search/StringBuilderReverse.java
@@ -0,0 +1,6 @@
+// StringBuilder Problem 1: Reverse a String
+public class StringBuilderReverse {
+    public static String reverse(String input) {
+        return new StringBuilder(input).reverse().toString();
+    }
+}
